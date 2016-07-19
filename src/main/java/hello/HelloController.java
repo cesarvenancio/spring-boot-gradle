fb26c010f271f55de36b40a7aa0c22f5241353c9
cesarvenancio <dolfo.venancio@gmail.com>
2016-07-19 19:42:34 +0100

    First config

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -2,13 +2,25 @@ package hello;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
 
+@Component
 @RestController
 public class HelloController {
+
+    private final JdbcTemplate jdbcTemplate;
 
     @RequestMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
     }
+
+    @Autowired
+    public HelloController(JdbcTemplate jdbcTemplate){
+    	this.jdbcTemplate = jdbcTemplate;
+    }
+
 
 }
